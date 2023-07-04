dce130af0a863e90f3f3860d70982f5ff41aef65
Prudence <Prudence@Shreyas-MacBook-Pro.local>
2023-07-04 13:27:07 +0700

    init commit - resolve cros

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -10,7 +11,7 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 // view engine setup
-
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
